cc1983ef31ff3f33cba94b0ee9ee7c0adcdb913d
Aman Yadav <192107aman@gmail.com>
2026-08-18 13:06:46 +0530

    Connect frontend to live backend

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://expense-tracker-api-m6hs.onrender.com/api';
 const TOKEN_KEY = 'ledger-token';
 
 export function getToken() {
